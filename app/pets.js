e644cbfdaba5d4cff137db3dbb528a3f3f28deaa
AbuSufianIslam <thebooming1992@gmail.com>
2020-01-05 21:14:48 -0500

    COmplete

--- app/pets.js
+++ app/pets.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {List} from './utils'
+import React from 'react';
+import { connect } from 'react-redux';
+import { List } from './utils';
+import { getPets } from './store';
 
 class Pets extends React.Component {
-  render () {
-    const {pets} = this.props
+	componentDidMount() {
+		this.props.getPets();
+	}
+	render() {
+		const { pets } = this.props;
 
-    return (
-      <div>
-        <List
-          forEachOfThese={pets}
-          doThis={pet => (
-            <div key={pet.id}>
-              <img src={pet.imageUrl} />
-              <p>{pet.name}</p>
-            </div>
-          )}
-          unlessEmpty={() => <div>Where are the pets?!?</div>}
-        />
-      </div>
-    )
-  }
+		return (
+			<div>
+				<List
+					forEachOfThese={pets}
+					doThis={(pet) => (
+						<div key={pet.id}>
+							<img src={pet.imageUrl} />
+							<p>{pet.name}</p>
+						</div>
+					)}
+					unlessEmpty={() => <div>Where are the pets?!?</div>}
+				/>
+			</div>
+		);
+	}
 }
 
 const mapState = (state) => {
-  return {
-    pets: state.pets
-  }
-}
+	return {
+		pets: state.pets
+	};
+};
 
 const mapDispatch = (dispatch) => {
-  // YOUR CODE HERE
-}
+	// YOUR CODE HERE
+	return {
+		getPets: () => dispatch(getPets())
+	};
+};
 
-export default connect(mapState, mapDispatch)(Pets)
+export default connect(mapState, mapDispatch)(Pets);
